d6cd0d5aaaff2fe844abb0016df847a6348d6f72
weswhitney <wesleywhitney@gmail.com>
2014-10-15 14:15:49 -0600

    updated code to a make quote object

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,20 @@
 class PagesController < ApplicationController
 
   def index
-    @quotes = ["foobar", "baz", "w00t"]
+
+    quote1 = Quote.new
+    quote1.text = "foobar"
+    quote1.author = "jeff"
+
+    quote2 = Quote.new
+    quote2.text = "baz"
+    quote2.author = "Steve"
+
+    quote3 = Quote.new
+    quote3.text = "w00t"
+    quote3.author = "Lark"
+
+    @quotes = [quote1, quote2, quote3]
   end
 
 end
