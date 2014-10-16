374c04f70d873194ea72830489c51c735658c047
weswhitney <wesleywhitney@gmail.com>
2014-10-16 13:14:00 -0600

    Lots of small tweaks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,4 +17,21 @@ class PagesController < ApplicationController
     @quotes = [quote1, quote2, quote3]
   end
 
+  def faq
+
+    faq1 = Faq.new
+    faq1.question = "What is gCamp?"
+    faq1.answer = "gCamp is an awesome tool that will change your life."
+
+    faq2 = Faq.new
+    faq2.question = "How do I get join gCamp?"
+    faq2.answer = "Right now, gCamp is still in production."
+
+    faq3 = Faq.new
+    faq3.question = "When will gCamp be finished?"
+    faq3.answer = "gCamp is a work in progress."
+
+    @faqs = [faq1, faq2, faq3]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root "pages#index"
 
-  get '/about' => 'pages#about', as: :about
-  get '/terms' => 'pages#terms', as: :terms
+  get 'about' => 'pages#about', as: :about
+  get 'terms' => 'pages#terms', as: :terms
   get 'faq' => 'pages#faq', as: :faq
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
