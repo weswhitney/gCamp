2dfa7d6640997e9a164cacd0129ff926b6b30511
weswhitney <wesleywhitney@gmail.com>
2014-10-28 13:14:05 -0600

    redirected to users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,16 +13,16 @@ class UsersController < ApplicationController
 
   def show
   end
-  
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email_address))
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
       respond_to do |format|
         if @user.save
-          format.html { redirect_to @user, notice: 'Task was successfully created.'}
+          format.html { redirect_to users_path, notice: 'User was successfully created.'}
           format.json { render :show, status: :created, location: @user }
         else
           format.html { render :new }
