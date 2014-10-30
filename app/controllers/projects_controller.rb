1d83c10404f8fff12a8d7a720fb547c973f4a930
weswhitney <wesleywhitney@gmail.com>
2014-10-30 15:57:46 -0600

    fixed users name link

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,38 @@
+class ProjectsController < ApplicationController
+
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(params.require(:project).permit(:name))
+    @project.save
+    redirect_to projects_path, notice: 'Project was successfully created.'
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    project_params = params.require(:project).permit(:name)
+    @project.update(project_params)
+    redirect_to projects_path, notice: 'Project was successfully updated.'
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: 'Project was successfully deleted'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :projects
+
   root "pages#index"
 
   get 'about' => 'pages#about', as: :about
