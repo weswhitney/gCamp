9ffcae7ed0b9b46178e77d9ca337debcedc7ed80
weswhitney <wesleywhitney@gmail.com>
2014-11-07 15:11:04 -0700

    capybara coverage to users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -10,53 +10,54 @@ feature "Users" do
     fill_in "Last name", with: "Example"
     fill_in "Email", with: "jobe@example.com"
     fill_in "Password", with: "pass"
-    fill_in "Password Confirmation", with: "pass"
+    fill_in "Password confirmation", with: "pass"
     click_on "Create User"
 
     expect(page).to have_content("User was successfully created")
+
+  end
+
+  scenario "User sees show user" do
+
+    User.create!(
+      first_name: "Jobe", last_name: "Example", email: "gob@job.com", password_digest: "1234"
+    )
+
+    visit "/users"
+    click_on "Jobe Example"
     expect(page).to have_content("Jobe")
 
   end
 
-  # scenario "User sees show user" do
-  #
-  #   User.create!(
-  #     first_name: "Jobe" , last_name: "Example" , email: "gob@job.com"
-  #   )
-  #
-  #   visit "/users"
-  #   click_on "Show"
-  #   expect(page).to have_content("Jobe")
-  #
-  # end
-  #
-  # scenario "User edits a user" do
-  #
-  #   User.create!(
-  #     first_name: "Jobe" , last_name: "Example" , email: "gob@job.com"
-  #   )
-  #
-  #   visit "/users"
-  #   click_on "Edit"
-  #   click_on "Update User"
-  #
-  #   expect(page).to have_content("User was successfully updated")
-  #   expect(page).to have_content("Jobe")
-  #
-  # end
-  #
-  # scenario "User deletes a user" do
-  #
-  #   User.create!(
-  #     first_name: "Jobe" , last_name: "Example" , email: "gob@job.com"
-  #   )
-  #
-  #
-  #   visit "/users"
-  #   click_on "Delete"
-  #   expect(page).to have_content("User was successfully destroyed")
-  #   expect(page).to have_no_content("Jobe")
-  #
-  # end
-  #
+  scenario "User edits a user" do
+
+    User.create!(
+      first_name: "Jobe" , last_name: "Example" , email: "gob@job.com", password_digest: "1234"
+    )
+
+    visit "/users"
+    click_on "edit"
+    click_on "Update User"
+
+    expect(page).to have_content("User was successfully updated")
+    expect(page).to have_content("Jobe")
+
+  end
+
+  scenario "User deletes a user" do
+
+    User.create!(
+      first_name: "Jobe" , last_name: "Example" , email: "gob@job.com", password_digest: "1234",
+    )
+
+
+    visit "/users"
+    click_on "Jobe Example"
+    click_on "Edit"
+    click_on "delete user"
+    expect(page).to have_content("User was successfully deleted")
+    expect(page).to have_no_content("Jobe")
+
+  end
+
 end
