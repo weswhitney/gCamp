4212767bdac2b3ceeaca32845731cfb0dcb6ad0f
weswhitney <wesleywhitney@gmail.com>
2014-11-11 22:45:41 -0700

    capybara coverage for blank projects name

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,59 +2,69 @@ require 'rails_helper'
 
 feature "Projects" do
 
-scenario "User creates a project" do
+  scenario "User creates a project" do
 
-  visit "/projects"
-  click_on "Create Project"
-  fill_in "Name", with: "Awes proj"
-  click_on "Create Project"
+    visit "/projects"
+    click_on "Create Project"
+    fill_in "Name", with: "Awes proj"
+    click_on "Create Project"
 
-  expect(page).to have_content("Project was successfully created.")
+    expect(page).to have_content("Project was successfully created.")
 
-end
+  end
 
-scenario "User sees project" do
+  scenario "User tries to create a project with no name" do
 
-  Project.create!(
-    name: "Awesome"
-  )
+    visit "/projects"
+    click_on "Create Project"
+    click_on "Create Project"
 
-  visit "/projects"
-  click_on "Awesome"
-  expect(page).to have_content("Awesome")
+    expect(page).to have_content("Name can't be blank")
 
-end
+  end
 
-scenario "User edits a user" do
+  scenario "User sees project" do
 
-  Project.create!(
-    name: "Awesome"
-  )
+    Project.create!(
+      name: "Awesome"
+    )
 
-  visit "/projects"
-  click_on "edit"
-  fill_in "Name", with: "Awesomeness"
-  click_on "Update Project"
+    visit "/projects"
+    click_on "Awesome"
+    expect(page).to have_content("Awesome")
 
-  expect(page).to have_content("Project was successfully updated")
-  expect(page).to have_content("Awesomeness")
+  end
 
-end
+  scenario "User edits a project" do
 
-scenario "User deletes a project" do
+    Project.create!(
+      name: "Awesome"
+    )
 
-  Project.create!(
-    name: "Awesome"
-  )
+    visit "/projects"
+    click_on "edit"
+    fill_in "Name", with: "Awesomeness"
+    click_on "Update Project"
+
+    expect(page).to have_content("Project was successfully updated")
+    expect(page).to have_content("Awesomeness")
+
+  end
+
+  scenario "User deletes a project" do
+
+    Project.create!(
+      name: "Awesome"
+    )
 
 
-  visit "/projects"
-  click_on "edit"
-  click_on "Update Project"
-  click_on "Delete"
-  expect(page).to have_content("Project was successfully deleted")
-  expect(page).to have_no_content("Awesome")
+    visit "/projects"
+    click_on "edit"
+    click_on "Update Project"
+    click_on "Delete"
+    expect(page).to have_content("Project was successfully deleted")
+    expect(page).to have_no_content("Awesome")
 
-end
+  end
 
 end
