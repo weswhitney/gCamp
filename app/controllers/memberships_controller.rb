4a981e5c1a0590a0af95d6d48bd41d3a6b4ea718
weswhitney <wesleywhitney@gmail.com>
2014-11-21 18:55:40 -0700

    added validations to memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -18,10 +18,17 @@ class MembershipsController < ApplicationController
     end
   end
 
+  def update
+    @membership = Membership.find(params[:id])
+    @membership.update(params.require(:membership).permit(:role, :project_id, :user_id))
+    redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was updated successfully"
+
+  end
+
   def destroy
     @membership = Membership.find(params[:id])
     @membership.destroy
-    redirect_to project_path(@project)
+    redirect_to project_path(@project), notice: "#{@membership.user.full_name} was removed successfully"
   end
 
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,8 @@
 class Membership < ActiveRecord::Base
 
   validates :user, presence: true
+  validates :user, uniqueness: {scope: :project}
+  validates :role, presence: true
   belongs_to :project
   belongs_to :user
 end
