d8618deba027c763c0ebe8e2b7d846de8325658f
weswhitney <wesleywhitney@gmail.com>
2014-11-26 13:27:30 -0700

    fixed form for comments and added null false to member

--- db/migrate/20141119235018_create_memberships.rb
+++ db/migrate/20141119235018_create_memberships.rb
@@ -1,9 +1,9 @@
 class CreateMemberships < ActiveRecord::Migration
   def change
     create_table :memberships do |t|
-      t.string :role
-      t.belongs_to :project
-      t.belongs_to :user
+      t.string :role, null: false
+      t.belongs_to :project, null: false
+      t.belongs_to :user, null: false
       t.timestamps
     end
   end
