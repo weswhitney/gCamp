e0d1f666b08a6e7bff033c0bf4198e4dad00c806
weswhitney <wesleywhitney@gmail.com>
2014-12-04 14:46:43 -0700

    when user creates new project they're automatically added as a project owner

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,20 +21,25 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(params.require(:project).permit(:name))
     if @project.save
-    redirect_to project_tasks_path(@project), notice: 'Project was successfully created.'
-  else
-    render :new
-  end
+      Membership.create(
+      project: @project,
+      user: current_user,
+      role: 'owner'
+      )
+      redirect_to project_tasks_path(@project), notice: 'Project was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
     @project = Project.find(params[:id])
     project_params = params.require(:project).permit(:name)
     if @project.update(project_params)
-    redirect_to project_path, notice: 'Project was successfully updated.'
-  else
-    render :edit
-  end
+      redirect_to project_path, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
