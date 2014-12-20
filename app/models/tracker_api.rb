b223951674c04e7709196a4d4e47fda55692591d
weswhitney <wesleywhitney@gmail.com>
2014-12-20 12:07:08 -0700

    deleted duplicate code on tracker_api model, added admin to edit user link

--- app/models/tracker_api.rb
+++ app/models/tracker_api.rb
@@ -28,18 +28,3 @@ class TrackerApi
     JSON.parse(response.body, symbolize_names: true)
   end
 end
-
-class PivotalProject
-  def initialize(id, name)
-    @id = id
-    @name = name
-  end
-
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-end
